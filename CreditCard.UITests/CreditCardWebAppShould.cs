ce5455c75f9450599ad72662e2da1578d7ece797
Rafal <grafal96@gmail.com>
2020-04-10 12:32:37 +0200

    Extended and refactored Smoke tests

--- CreditCard.UITests/CreditCardWebAppShould.cs
+++ CreditCard.UITests/CreditCardWebAppShould.cs
@@ -8,6 +8,8 @@ namespace CreditCard.UITests
 {
     public class CreditCardWebAppShould
     {
+        const string HomeUrl = "http://localhost:44108/";
+        const string HomeTitle = "Home Page - Credit Cards";
 
         [Fact]
         [Trait("Category", "Smoke")]
@@ -15,13 +17,26 @@ namespace CreditCard.UITests
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                const string homeUrl = "http://localhost:44108/";
+                driver.Navigate().GoToUrl(HomeUrl);
 
-                driver.Navigate().GoToUrl(homeUrl);
-
-                Assert.Equal("Home Page - Credit Cards", driver.Title);
-                Assert.Equal(homeUrl, driver.Url);
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
             }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                driver.Navigate().Refresh();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+
         }
     }
 }
