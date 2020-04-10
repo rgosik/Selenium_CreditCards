eacc3ffc4ad5d722af0bff5dc0ca3d602855dbfb
Rafal <grafal96@gmail.com>
2020-04-10 12:21:06 +0200

    Added Selenium project and wrote inital smoke test

---
+++ CreditCard.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace CreditCard.UITests
+{
+    public class CreditCardWebAppShould
+    {
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                const string homeUrl = "http://localhost:44108/";
+
+                driver.Navigate().GoToUrl(homeUrl);
+
+                Assert.Equal("Home Page - Credit Cards", driver.Title);
+                Assert.Equal(homeUrl, driver.Url);
+            }
+        }
+    }
+}
